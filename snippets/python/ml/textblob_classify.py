d0ee730d078f5ec7ed0a169bb6f792430dd25c63
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2019-12-25 09:07:20 +0300

    Create textblob_classify.py

---
+++ snippets/python/ml/textblob_classify.py
@@ -0,0 +1,15 @@
+import pickle
+
+from textblob.classifiers import NaiveBayesClassifier
+
+classifier = NaiveBayesClassifier([(..., ...), ...])
+
+classifier.update([(..., ...), ...])
+
+with open('...', 'wb') as f:
+    pickle.dump(classifier, f)
+
+with open('...', 'rb') as f:
+    classifier = pickle.load(f)
+
+classifier.classify(...)
