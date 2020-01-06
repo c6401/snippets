e4c08c2c36ba512f67568157e14b0ac884406e14
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-01-07 00:02:24 +0300

    Create setup.py

---
+++ snippets/python/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='...',
+    version='0.1',
+    description='...',
+    author='...',
+    author_email='...',
+    url='https://github.com/',
+    packages=['...'],
+    license='MIT',
+    install_requires=[
+        '...',
+    ],
+    # scripts=['...']
+)
