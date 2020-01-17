55f722f2392212f6f9e6115b0c35bde071f30421
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-01-18 01:31:54 +0300

    Create in_chunks.py

---
+++ snippets/python/utils/in_chunks.py
@@ -0,0 +1,9 @@
+def in_chunks(seq, size):
+    chunk = []
+    for n, item in enumerate(seq, 1):
+        chunk.append(item)
+        if n % size == 0:
+            yield chunk
+            chunk = []
+    if chunk:
+        yield chunk 
