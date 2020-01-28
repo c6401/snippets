0e3c60621ee60666a2e9988a8f7fce03441ca2e4
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-01-28 08:31:07 +0300

    Update motor_mongo.py

--- snippets/python/database/motor_mongo.py
+++ snippets/python/database/motor_mongo.py
@@ -1,11 +1,15 @@
 import motor.motor_asyncio
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
+mongo = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017')
 
-collection = client.bd.collection
+collection = mongo.bd.collection
 
 await collection.insert_one({
 })
+
+await collection.find_one()
+
+await collection.delete_one({})
 
 for document in await collection.find().to_list(length=100):
     ...
