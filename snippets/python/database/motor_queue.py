53c280352e326446c25b6909128269bcfe8b6b76
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-01-30 01:04:06 +0300

    Create motor_queue.py

---
+++ snippets/python/database/motor_queue.py
@@ -0,0 +1,31 @@
+import asyncio
+import logging
+# create mongo
+
+async def queue(consumer: str, topic: str, query={}, interval=5 * 60):
+    offset = mongo.common.offset
+    last = await offset.find_one({'consumer': consumer, 'topic': topic})
+    query = query.copy()
+    if last:
+        query.update({'_id': {'$gt': last['offset']}})
+    collection = mongo.common[topic]
+
+    while True:
+        cursor = collection.find(query)
+        logging.info(f'{consumer} is iterating over {topic}')
+        result = None
+        results = await cursor.to_list(length=100)
+        for result in results:
+            yield result
+
+            await offset.update_one(
+                {'consumer': consumer, 'topic': topic},
+                {'$set': {'offset': result['_id']}},
+                True,
+            )
+        if result:
+            query.update({'_id': {'$gt': result['_id']}})
+
+        if not results:
+            logging.info(f'{consumer} to {topic} sub is going to sleep')
+            await asyncio.sleep(interval)
