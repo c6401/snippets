54d5fdaf2f14e36336b9e91b5fa3e93c2bd7e48b
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-02-03 00:09:37 +0300

    Create drf_view.py

---
+++ snippets/python/django/prototype/drf_view.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework import views
+from rest_framework.response import Response
+
+
+class MyView(views.APIView):
+    def get(self, request, format=None):
+        return Response({'test': 'test'})
+
+
+mypath = path('myview', MyView.as_view())
+if urlpatterns:
+    urlpatterns[0] = mypath
+else:
+    urlpatterns.append(mypath)
