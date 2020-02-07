101cab9cf116a9c4cc89d50f223d0e7e774cbb81
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-02-08 00:08:56 +0300

    Update puppeteer with cookies.py

--- snippets/python/http/puppeteer/puppeteer with cookies.py
+++ snippets/python/http/puppeteer/puppeteer with cookies.py
@@ -3,16 +3,20 @@ from urllib.parse import urlparse
 
 from browsercookie import firefox as get_jar
 from pyppeteer import launch
+from pyppeteer_stealth import stealth
 
 async def func():
     url = 'https://...'
     browser = await launch({'headless': False})
     page = await browser.newPage()
+    await stealth(page)
     await page.setCookie(*[
         dict(name=c.name, value=c.value, domain=c.domain)
         for c in get_jar() if urlparse(url).hostname in c.domain
     ])
     await page.goto(url)
+
+    # element = (await page.xpath('//...'))[0]
 
     await browser.close()
 
