200afd184fd91cddeeb25de3f57265b3bc7ffe5c
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-02-09 01:19:20 +0300

    Update puppeteer.py

--- snippets/python/http/puppeteer/puppeteer.py
+++ snippets/python/http/puppeteer/puppeteer.py
@@ -1,11 +1,19 @@
 import asyncio
 
 from pyppeteer import launch
+# from pyppeteer_stealth import stealth
 
 async def func():
-    browser = await launch()
-    page = await browser.newPage({'headless': False})
+    browser = await launch({'headless': False})
+    page = await browser.newPage()
+    # await stealth(page)
     await page.goto('...')
+
+    # await page.evaluate(
+    #     f'''document.evaluate("//*[contains(text(), '...')]", '''
+    #     '''document, null, XPathResult.ANY_TYPE, null).iterateNext().click()'''
+    # )
+    # await page.evaluate(f'''document.querySelector('...').click()''')
 
     await browser.close()
 
