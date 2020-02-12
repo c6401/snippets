bbb4710162ea467b35bad0341549502e086968eb
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-02-13 00:25:15 +0300

    Update define_model.py

--- snippets/python/django/prototype/define_model.py
+++ snippets/python/django/prototype/define_model.py
@@ -10,3 +10,12 @@ class MyModel(models.Model):
 
     class Meta:
         app_label = 'myapp'
+
+# from django.core import management
+# management.call_command('makemigrations', 'myapp')
+# management.call_command('migrate', 'myapp')
+
+from django.db import connection
+
+with connection.schema_editor() as schema_editor:
+    schema_editor.create_model(MyModel)
