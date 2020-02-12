282fdffbeb03ceccc5005915d5965beeb0c554ad
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-02-13 00:25:19 +0300

    Delete create_tables.py

--- snippets/python/django/prototype/create_tables.py
+++
@@ -1,8 +0,0 @@
-# from django.core import management
-# management.call_command('makemigrations', 'myapp')
-# management.call_command('migrate', 'myapp')
-
-from django.db import connection
-
-with connection.schema_editor() as schema_editor:
-    schema_editor.create_model(MyModel)
