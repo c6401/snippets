589bbcb53ad4beb340a2e48b4645a03d334af2a2
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-09-14 00:36:22 +0300

    Cleanup

--- snippets/operations/dockerfile/deb/create-user.dockerfile
+++ snippets/operations/dockerfile/deb/create-user.dockerfile
@@ -1,5 +1,5 @@
 RUN useradd -m -s /bin/bash -d /opt/app --uid $UID --gid $GID --groups sudo app
 RUN echo "app:secret" | chpasswd
 
-WORKDIR /opt/app/unhaggle
+WORKDIR /opt/app/app
 USER app
--- snippets/python/api/slack/message.py
+++ snippets/python/api/slack/message.py
@@ -1,7 +1,7 @@
 import requests
 
 requests.post(
-    "https://unhaggle-chat.slack.com/api/chat.postMessage",
+    "https://....slack.com/api/chat.postMessage",
     data={
         'channel': ...,
         'text': ...,
