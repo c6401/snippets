aae1a2340be19fcec064813c0bf67f63a1ebe820
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-10-05 19:30:08 +0300

    Update call shell.py

--- snippets/python/interface/call shell.py
+++ snippets/python/interface/call shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-def sh(command, capture=True, **kwargs):
+def cmd(command, capture=True, **kwargs):
     if capture: kwargs.update(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     process = subprocess.Popen(command, shell=True, **kwargs)
     (stdout, stderr) = process.communicate()
