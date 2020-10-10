15f0f6d228f46239a8a95dc9e48754396b9ce823
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-10-11 02:08:03 +0300

    Update shelve_cache.py

--- snippets/python/utils/shelve_cache.py
+++ snippets/python/utils/shelve_cache.py
@@ -1,10 +1,12 @@
 import shelve
 from functools import wraps
 
-def shelve_cache(
-    path=f'/tmp/{__file__}.shelve',
-    keyfunc=lambda func, *args, **kwargs: str((func.__name__, args, kwargs))
-):
+def shelve_cache(path=None, keyfunc=None):
+    if not path:
+        path = '/tmp/' + __file__.rsplit("/", 1)[-1] + '.shelve'
+    if not keyfunc:
+        keyfunc = lambda f, *a, **kw: str((f.__name__, a, kw))
+
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -17,3 +19,5 @@ def shelve_cache(
 
     decorator.shelve = shelve.open(path)
     return decorator
+
+cache = shelve_cache()
