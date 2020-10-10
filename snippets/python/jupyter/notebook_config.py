35d4ee464ceb48cde89bde9fd1302356428b8e07
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-10-11 02:08:37 +0300

    Create notebook_config.py

---
+++ snippets/python/jupyter/notebook_config.py
@@ -0,0 +1,10 @@
+c = get_config()
+
+# c.NotebookApp.allow_root = True
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+c.NotebookApp.allow_remote_access = True
+c.NotebookApp.password_required = False
+c.NotebookApp.password = ''
+c.NotebookApp.token = ''
