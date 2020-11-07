b3b042e6b8b8ca7641753510b96423867d6f5d2f
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2020-11-07 10:49:54 +0300

    Create call shell.py

---
+++ snippets/python/automation/call shell.py
@@ -0,0 +1,11 @@
+import subprocess
+
+
+def cmd(command, capture=True, **kwargs):
+    if capture:
+        kwargs.update(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(command, shell=True, **kwargs)
+    (stdout, stderr) = process.communicate()
+    if process.returncode:
+        exit((stderr or b'').decode('utf-8'))
+    return (stdout or b'').decode('utf-8').strip()
