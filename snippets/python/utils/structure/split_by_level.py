bbe038fe50d4531be36613e0ce2bc2e86161931e
c6401 <nc6401+gthb@gmail.com>
2021-01-17 17:43:02 +0300

    Update split_by_level.py

--- snippets/python/utils/structure/split_by_level.py
+++ snippets/python/utils/structure/split_by_level.py
@@ -2,11 +2,11 @@ import re
 
 def split_by_level(lines, symbol=' ', sep='', indented=True):
     contents = []
-    block_level = 0
-    prev_level = 0
-    level_re = re.compile(f'(?:({re.escape(symbol)}+){re.excapt(sep)})?')
+    block_level = prev_level = level = 0
+    level_re = re.compile(f'(?:({re.escape(symbol)}+){re.escape(sep)})?')
     for line in lines:
-        level = len(level_re.match(line).groups()[0] or '')
+        if line.strip():
+            level = len(level_re.match(line).groups()[0] or '')
         if (indented and prev_level != level) or (not indented and level):
             yield block_level, contents
             contents = []
