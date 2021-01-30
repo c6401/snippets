fd9583d9e57e3172f39f2b267566a1b6b4cdda88
c6401 <nc6401+gthb@gmail.com>
2021-01-30 23:19:55 +0300

    Update build_tree.py

--- snippets/python/utils/structure/build_tree.py
+++ snippets/python/utils/structure/build_tree.py
@@ -1,20 +1,24 @@
+"""
+>>> build_tree([(1, 'a'), (3, 'b'), (2, 'c')])
+[{'data': 'a', 'children': [{'data': 'b', 'children': [], 'level': 3}, \
+{'data': 'c', 'children': [], 'level': 2}], 'level': 1}]
+"""
+
 def build_tree(pairs, factory=dict):
     """Builds nested tree from sequence of pairs: [(nest-level, data), ...]."""
-    root = siblings = previous_node_children = []
+    root = siblings = children = []
     previous_level = -1  # root level
-    parents = []
+    parents = []  # stack of parents
 
     for level, data in pairs:
-        children = []
-        node = factory(level=level, children=children, data=data)
         if level > previous_level:
-            siblings = previous_node_children
-            parents.append((previous_level, previous_node_children))
+            siblings = children  # prev node's children are current's siblings
+            parents.append((previous_level, children))
         elif level < previous_level:
-            while level <= parents[-1][0]:  # parent's level
+            while level <= parents[-1][0]:  # child's level should be > parent's
                 parents.pop()
-            siblings = parents[-1][1]  # parent's children
-        siblings.append(node)
+            siblings = parents[-1][1]  # set siblings as parent's children
+        children = []
+        siblings.append(factory(level=level, children=children, data=data))
         previous_level = level
-        previous_node_children = children
     return root
