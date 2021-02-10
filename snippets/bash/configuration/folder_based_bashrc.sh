22062ea53925bbf26077950927d4dffb993188a8
c6401 <nc6401+gthb@gmail.com>
2021-02-10 05:19:19 +0300

    Update folder_based_bashrc.sh

--- snippets/bash/configuration/folder_based_bashrc.sh
+++ snippets/bash/configuration/folder_based_bashrc.sh
@@ -1,9 +1,29 @@
-if [ -f ~/.bashrc ]; then
-    . ~/.bashrc
+touch ~/.bashrc.whitelist
+
+function _source_rc () {
+    if grep -Fxq "$PWD" ~/.bashrc.whitelist
+    then
+        source .bashrc
+    else
+        read -p "Whitelist $PWD/.bashrc file? " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            echo $PWD >> ~/.bashrc.whitelist
+            source .bashrc
+        fi
+    fi
+}
+
+if [ -f .bashrc ] && [ "$PWD" != "$HOME" ]
+then
+    _source_rc
 fi
+
 function cd () {
     builtin cd "$@"
-    if [ -f ~/.bashrc ]; then
-        . ~/.bashrc
+    if [ -f .bashrc ]
+    then
+        _source_rc
     fi
 }
