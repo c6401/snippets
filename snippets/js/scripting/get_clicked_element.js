d4989512ec1005cb390dc10fd57a745dd7b972ae
c6401 <nc6401+gthb@gmail.com>
2021-02-14 11:50:55 +0300

    Update get_clicked_element.js

--- snippets/js/scripting/get_clicked_element.js
+++ snippets/js/scripting/get_clicked_element.js
@@ -1,5 +1,8 @@
-function listener(event) {
+document.addEventListener('click', function listener(event) {
   console.log(event.target);
+  console.log(event.target.parentNode.parentNode.parentNode);
   document.removeEventListener('click', listener, true);
-}
-document.addEventListener('click', listener, true);
+  event.preventDefault();
+  event.stopPropagation();
+  return false;
+}, true);
