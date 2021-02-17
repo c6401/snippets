8e804758437445056933e58b0f208fb7985c18dc
c6401 <nc6401+gthb@gmail.com>
2021-02-17 17:07:48 +0300

    Create shortcut.js

---
+++ snippets/js/scripting/shortcut.js
@@ -0,0 +1,7 @@
+(function(){
+document.addEventListener('keydown', function(e) {
+  if (e.keyCode == 'a'.charCodeAt(0) && e.shiftKey && e.ctrlKey && e.altKey && e.metaKey) {
+
+  }
+}, false);
+})();
