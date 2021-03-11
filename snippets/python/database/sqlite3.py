eb5fe031b1d7eacb57115071c51ec77e55e3d219
c6401 <nc6401+gthb@gmail.com>
2021-03-11 19:13:05 +0300

    Update sqlite3.py

--- snippets/python/database/sqlite3.py
+++ snippets/python/database/sqlite3.py
@@ -1,6 +1,8 @@
 import sqlite3
 
 with sqlite3.connect('...') as connection:
-    cursor = connection.execute('select * from ...')
+    cursor = connection.execute('select 1;')
+    # cursor = connection.execute("SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%';")
+    # cursor = connection.execute('select ?', [...])
     records = cursor.fetchall()
 #cursor.description
