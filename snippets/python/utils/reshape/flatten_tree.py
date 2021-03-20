4f11895a2c6ce31039d44eabe1653c4810025fda
c6401 <nc6401+gthb@gmail.com>
2021-03-20 06:24:34 +0300

    Move ungroup.py

--- snippets/python/utils/reshape/flatten_tree.py
+++ snippets/python/utils/reshape/flatten_tree.py
@@ -1,7 +1,7 @@
-def ungroup_records(tree):
+def flatten_tree(tree):
     if isinstance(tree, dict):
         for item, group in tree.items():
-            for record in ungroup(group):
+            for record in flatten_tree(group):
                 yield (item, *record)
     else:
         yield (tree,)
