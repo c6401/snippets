59a7a9e1ca6c1789bf0396b6cf987cb1211691cb
c6401 <nc6401+gthb@gmail.com>
2021-03-27 12:06:26 +0300

    Create App.vue

---
+++ snippets/js/vue/compiled/App.vue
@@ -0,0 +1,18 @@
+<template>
+  {{ message }}
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      message: "hello"
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
