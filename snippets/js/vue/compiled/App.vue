638a0d532389382bc08d95a4009df8f75112fce1
c6401 <nc6401+gthb@gmail.com>
2021-04-02 21:48:44 +0300

    Update App.vue

--- snippets/js/vue/compiled/App.vue
+++ snippets/js/vue/compiled/App.vue
@@ -1,5 +1,7 @@
 <template>
-  {{ message }}
+  <div>
+    {{ message }}
+  </div>
 </template>
 
 <script>
@@ -8,7 +10,7 @@ import Vue from "vue";
 export default Vue.extend({
   data() {
     return {
-      message: "hello"
+      message: "hello",
     };
   }
 });
