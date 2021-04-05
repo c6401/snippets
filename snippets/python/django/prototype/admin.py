4ac9029d7837f10333946b7106c477050efa1462
c6401 <nc6401+gthb@gmail.com>
2021-04-05 16:42:40 +0300

    Update admin.py

--- snippets/python/django/prototype/admin.py
+++ snippets/python/django/prototype/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+from django.urls import path
+
 
 class MyModelAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(MyModel, MyModelAdmin)
 
-
-from django.urls import path
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+call_command('migrate')
+User = get_user_model()
+User.objects.create_superuser(username='admin', email='', password='admin')
