1342e7424c3c44f5fc30eaa1ba5ad2da77a4ea40
c6401 <nc6401+gthb@gmail.com>
2021-04-17 15:37:42 +0300

    Move notebook_config.py

--- snippets/python/jupyter/jupyter_notebook_config.py
+++ snippets/python/jupyter/jupyter_notebook_config.py
@@ -1,8 +1,9 @@
+# jupyter_notebook_config.py in $JUPYTER_CONFIG_DIR
 c = get_config()
 
 # c.NotebookApp.allow_root = True
 c.NotebookApp.ip = '*'
-c.NotebookApp.port = 8888
+c.NotebookApp.port = 8888  # NOTE don't forget to open
 c.NotebookApp.open_browser = False
 c.NotebookApp.allow_remote_access = True
 c.NotebookApp.password_required = False
