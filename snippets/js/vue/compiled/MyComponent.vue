24fe4f789691e25c473ce9d004590c1e782d6ca7
c6401 <nc6401+gthb@gmail.com>
2021-04-20 20:41:01 +0300

    Create MyComponent.vue

---
+++ snippets/js/vue/compiled/MyComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    hello
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'MyComponent',
+  // props: {
+  //   myProp: {
+  //     type: String,
+  //     required: true,
+  //   },
+  // },
+  computed: {
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
