7ec993abcae03203104faa2fe3d6f4ae8a73794f
c6401 <nc6401+gthb@gmail.com>
2021-06-22 19:21:59 +0300

    Update browsercookie.py

--- snippets/python/network/puppeteer/browsercookie.py
+++ snippets/python/network/puppeteer/browsercookie.py
@@ -1,23 +1,21 @@
-import asyncio
 from urllib.parse import urlparse
 
-from browsercookie import firefox as get_jar
+from browsercookie import firefox
 from pyppeteer import launch
-from pyppeteer_stealth import stealth
+# from pyppeteer_stealth import stealth
 
-async def func():
-    url = 'https://...'
-    browser = await launch({'headless': False})
-    page = await browser.newPage()
-    await stealth(page)
-    await page.setCookie(*[
-        dict(name=c.name, value=c.value, domain=c.domain)
-        for c in get_jar() if urlparse(url).hostname in c.domain
-    ])
-    await page.goto(url)
+cookies = firefox(cookie_file=[''])
 
-    # element = (await page.xpath('//...'))[0]
+url = 'https://...'
+browser = await launch({'headless': False})
+page = await browser.newPage()
+# await stealth(page)
+await page.setCookie(*[
+    dict(name=c.name, value=c.value, domain=c.domain)
+    for c in cookies if urlparse(url).hostname in c.domain
+])
+await page.goto(url)
 
-    await browser.close()
+# element = (await page.xpath('//...'))[0]
 
-asyncio.get_event_loop().run_until_complete(func())
+await browser.close()
