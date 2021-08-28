f5075ccebe375433dda6ccfd049772edc6f1a64d
c6401 <nc6401+gthb@gmail.com>
2021-08-28 13:26:19 +0300

    Update ff_cookie.py

--- snippets/python/network/ff_cookie.py
+++ snippets/python/network/ff_cookie.py
@@ -6,7 +6,7 @@ domain = ...
 profiles = Path('~/.mozilla/firefox').expanduser().glob('*.default*')
 profile = sorted(profiles, key=lambda p: not str(p).endswith('-release'))[0]
 copyfile(profile / 'cookies.sqlite', '/tmp/db.sqlite')
+# id originAttributes name value host path expiry lastAccessed creationTime isSecure isHttpOnly inBrowserElement sameSite rawSameSite schemeMap
 with sqlite3.connect('/tmp/db.sqlite') as c:
-    # id originAttributes name value host path expiry lastAccessed creationTime isSecure isHttpOnly inBrowserElement sameSite rawSameSite schemeMap
-    cookies = c.execute('select * from moz_cookies where host like ?', [f'%{domain}']).fetchall()
-COOKIES = {c[2]: c[3] for c in cookies}
+    result = c.execute('select * from moz_cookies where host like ?', [f'%{domain}']).fetchall()
+cookies = {c[2]: c[3] for c in result}
