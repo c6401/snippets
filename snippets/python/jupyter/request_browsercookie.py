a9038dad38cc0cf86366f7981eb32456dcedb338
c6401 <nc6401+gthb@gmail.com>
2021-08-29 08:13:20 +0300

    Delete request_browsercookie.py

--- snippets/python/jupyter/request_browsercookie.py
+++
@@ -1,9 +0,0 @@
-import requests
-import browsercookie
-from IPython.display import HTML
-
-cookies = browsercookie.firefox(cookie_file=[''])
-
-response = requests.get('https://', cookies=cookies)
-
-display(response.text)
