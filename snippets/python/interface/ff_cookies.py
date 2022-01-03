03319415e72e90a522cdebf177a06fef7462fd88
c6401 <nc6401+gthb@gmail.com>
2022-01-03 13:50:48 +0300

    Update ff_cookies.py

--- snippets/python/interface/ff_cookies.py
+++ snippets/python/interface/ff_cookies.py
@@ -4,10 +4,9 @@ import sqlite3
 
 domain = '...'
 
-profiles = Path('~/.mozilla/firefox').expanduser().glob('*.default*')
-profile = sorted(profiles, key=lambda p: not str(p).endswith('-release'))[0]
+profile = next(Path('~/.mozilla/firefox').expanduser().glob('*.default*'))
 copyfile(profile / 'cookies.sqlite', '/tmp/db.sqlite')
 with sqlite3.connect('/tmp/db.sqlite') as c:
-    cookies = c.execute('select * from moz_cookies where host like ?', [f'%{domain}']).fetchall()
-COOKIES = {c[2]: c[3] for c in cookies}
+    items = c.execute('select * from moz_cookies where host like ?', [f'%{domain}']).fetchall()
+cookies = {i[2]: i[3] for i in items}
 
