57133ca103e26c185561e597c6b8a6771cc8d6eb
c6401 <nc6401+gthb@gmail.com>
2022-01-22 07:32:46 +0300

    Update walk_tree.js

--- snippets/js/utils/walk_tree.js
+++ snippets/js/utils/walk_tree.js
@@ -1,13 +1,13 @@
 function walk(obj) {
-    if (Array.isArray(obj)) {
-        return obj.map(val => walk(val));
-    } else if (obj instanceof Object) {
-        const result = {};
-        Object.entries(obj).forEach(([key, val]) => {
-            result[key] = walk(val);
-        });
-        return result;
-    } else {
-       return obj;
-    };
+  if (Array.isArray(obj)) {
+    return obj.map(val => walk(val));
+  } else if (obj instanceof Object) {
+    const result = {};
+    Object.entries(obj).forEach(([key, val]) => {
+      result[key] = walk(val);
+    });
+    return result;
+  } else {
+    return obj;
+  };
 }
