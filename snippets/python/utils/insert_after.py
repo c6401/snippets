3bcc71c29b2cebd7e6384c1084d2c0be205a802a
c6401 <nc6401+gthb@gmail.com>
2022-02-01 15:50:07 +0300

    Create insert_after.py

---
+++ snippets/python/utils/insert_after.py
@@ -0,0 +1,7 @@
+with open('____', 'r') as input:
+    lines = input.readlines()
+with open('____', 'w') as output:
+    for line in lines:
+        if line == '____':
+            line += '\n____'
+        output.write(line + '\n')
