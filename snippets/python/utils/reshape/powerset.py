26807034f0d057cca88e7a00c2b8e3d38ae6b65e
Ruslan Zhenetl <nc6401+gthb@gmail.com>
2022-09-11 19:18:44 +0300

    Create powerset.py

---
+++ snippets/python/utils/reshape/powerset.py
@@ -0,0 +1,8 @@
+from itertools import chain, combinations
+
+def powerset(iterable):
+    """Itertools powerset recipe.
+    powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)
+    """
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
