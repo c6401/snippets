d5d97778dbbb2ca8726e39946f3969b615955967
c6401 <nc6401+gthb@gmail.com>
2022-10-04 21:56:53 +0300

    Update pascal_to_snake.py

--- snippets/python/parsing_rendering/text/pascal_to_snake.py
+++ snippets/python/parsing_rendering/text/pascal_to_snake.py
@@ -1,5 +1,5 @@
 import re
 
-def pascal_to_snake(name):
-    result = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+def pascal_to_snake(value):
+    result = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', value)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', result).lower()
