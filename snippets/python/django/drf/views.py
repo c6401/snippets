9c92c44b0c86cc5ca494a9982d447e947a69954b
c6401 <nc6401+gthb@gmail.com>
2022-11-23 01:10:15 +0000

    Create views.py

---
+++ snippets/python/django/drf/views.py
@@ -0,0 +1,12 @@
+"""MyModelViewset."""
+from rest_framework import viewsets
+
+from . import models
+from . import serializers
+
+
+class MyModelViewset(viewsets.ModelViewSet):
+    """MyModelViewset."""
+
+    queryset = models.MyModel.objects.all()
+    serializer_class = serializers.MyModelSerializer
