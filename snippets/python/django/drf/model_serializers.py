8e2e74732513db4265fa1f063982a7e829aa4099
c6401 <nc6401+gthb@gmail.com>
2022-11-29 00:01:25 +0000

    Create model_serializers.py

---
+++ snippets/python/django/drf/model_serializers.py
@@ -0,0 +1,12 @@
+"""MyModel Serializer."""
+from rest_framework import serializers
+
+from . import models
+
+
+class MyModelSerializer(serializers.ModelSerializer):
+    """MyModel serializer"""
+
+    class Meta:
+        model = models.MyModel
+        fields = '__all__'
