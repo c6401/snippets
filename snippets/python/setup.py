0445b2fc44049e948e02f36c5833023beb51764f
c6401 <nc6401+gthb@gmail.com>
2022-11-29 00:01:38 +0000

    Update setup.py

--- snippets/python/setup.py
+++ snippets/python/setup.py
@@ -2,15 +2,16 @@ from distutils.core import setup
 
 setup(
     name='...',
-    version='0.1',
+    version='0.0.1',
     description='...',
+    long_description=open('README.md').read(),
     author='...',
     author_email='...',
     url='https://github.com/',
-    packages=['...'],
     license='MIT',
+    packages=['...'],
     install_requires=[
         '...',
     ],
-    # scripts=['...'],
+    scripts=['...'],
 )
