7f3f3315617946d530d185b450b7b6424df20768
c6401 <nc6401+gthb@gmail.com>
2022-12-13 12:13:07 +0000

    Delete mygit.sh

--- snippets/bash/configuration/mygit.sh
+++
@@ -1,4 +0,0 @@
-export MYGITDIR='.mygit'
-alias mg-here='export MYGITTREE="$PWD"'
-alias mg-init='git init --bare "$MYGITTREE/$MYGITDIR"'
-alias mg='/usr/bin/git --git-dir="$MYGITTREE/$MYGITDIR" --work-tree="$MYGITTREE"'
