b5c2badf082449e03a7be08f819c841d63281560
c6401 <nc6401+gthb@gmail.com>
2023-01-03 00:01:18 +0000

    Create keypress.js

---
+++ snippets/js/scripting/keypress.js
@@ -0,0 +1,12 @@
+window.dispatchEvent(new KeyboardEvent('keydown', {'key': 'a'}));
+
+// space 32
+// enter 13
+// escape 27
+// left 37
+// right 39
+// up 38
+// down 40
+// tab 9
+// delete 46
+// backspace 8
