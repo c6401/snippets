e1caf016838797978408dc8fd7c97832a5ba355b
c6401 <>
2024-02-26 00:07:26 +0000

    Refactor XMLHttpRequest logging in intercept_request.js

--- snippets/js/scripting/intercept_request.js
+++ snippets/js/scripting/intercept_request.js
@@ -1,17 +1,12 @@
 (function(open, setRequestHeader, send) {
     XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
-        this.addEventListener("readystatechange", function() {
-            if (this.readyState === 4) {
-                console.log(this);
-            };
-        }, false);
         const headers = {};
         XMLHttpRequest.prototype.setRequestHeader = function(name, value) {
             headers[name] = value;
             setRequestHeader.call(this, name, value);
         };
         XMLHttpRequest.prototype.send = function(body) {
-            console.log(method, url, async, user, password, headers, body);
+            console.log(JSON.stringify({method, url, async, user, password, headers, body}));
             send.call(this, body);
         };
         open.call(this, method, url, async, user, password);
