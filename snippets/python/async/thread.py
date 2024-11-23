13e8e6b19058691fb489a73d368d2b2a46a97a51
c6401 <nc6401+gthb@gmail.com>
2024-11-23 22:13:45 +0000

    Change run_in_thread function parameter order

--- snippets/python/async/thread.py
+++ snippets/python/async/thread.py
@@ -1,7 +1,7 @@
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
-def run_in_thread(func, _workers=1, *args, **kwargs):
+def run_in_thread(func, *args, _workers=1, **kwargs):
     with ThreadPoolExecutor(max_workers=_workers) as executor:
         loop = asyncio.get_event_loop()
         return loop.run_in_executor(executor, lambda: func(*args, **kwargs))
