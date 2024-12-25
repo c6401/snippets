1c109b97fb80b3ccaf82d4aaa511369d539868fb
c6401 <nc6401+gthb@gmail.com>
2024-12-25 00:37:40 +0000

    Add a generic decorator implementation for Python functions

---
+++ snippets/python/types/decorator.py
@@ -0,0 +1,6 @@
+from collections.abc import Callable
+
+def decorator[**P, R](func: Callable[P, R]) -> Callable[P, R]:
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        return func(*args, **kwargs)
+    return wrapper
