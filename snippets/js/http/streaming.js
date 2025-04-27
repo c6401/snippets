ea017dd685465799314345b9371f6c4c59b82250
c6401 <nc6401+gthb@gmail.com>
2025-04-27 06:27:59 +0100

    Implement streaming data fetch and logging from server

---
+++ snippets/js/http/streaming.js
@@ -0,0 +1,10 @@
+const response = await fetch("/stream");
+const reader = response.body.getReader();
+const decoder = new TextDecoder();
+
+while (true) {
+  const { done, value } = await reader.read();
+  if (done) break;
+  const chunk = decoder.decode(value);
+  console.log(chunk);
+}
