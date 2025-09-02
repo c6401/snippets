e688603a90a107292386e2f292f1af69c46aa43d
c6401 <nc6401+gthb@gmail.com>
2025-09-02 10:14:21 +0100

    Refactor XMLHttpRequest interception to improve readability and maintainability

--- snippets/js/automation/intercept_request.js
+++ snippets/js/automation/intercept_request.js
@@ -1,14 +1,15 @@
-(function(open, setRequestHeader, send) {
-    XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
-        const headers = {};
-        XMLHttpRequest.prototype.setRequestHeader = function(name, value) {
-            headers[name] = value;
-            setRequestHeader.call(this, name, value);
-        };
-        XMLHttpRequest.prototype.send = function(body) {
-            console.log(JSON.stringify({method, url, async, user, password, headers, body}));
-            send.call(this, body);
-        };
-        open.call(this, method, url, async, user, password);
+(function(XHR) {
+  const {open, setRequestHeader, send} = XHR.prototype;
+  XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+    const headers = {};
+    XMLHttpRequest.prototype.setRequestHeader = function(name, value) {
+      headers[name] = value;
+      setRequestHeader.call(this, name, value);
     };
-})(XMLHttpRequest.prototype.open, XMLHttpRequest.prototype.setRequestHeader, XMLHttpRequest.prototype.send);
+    XMLHttpRequest.prototype.send = function(body) {
+      console.log(JSON.stringify({method, url, async, user, password, headers, body}));
+      send.call(this, body);
+    };
+    open.call(this, method, url, async, user, password);
+  };
+})(XMLHttpRequest);
