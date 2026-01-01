c92e92e52dfd9380f55a94ece4d2f04e87480985
c6401 <nc6401+gthb@gmail.com>
2026-01-01 00:24:13 +0000

    Refactor XMLHttpRequest interception to use postMessage and add message event listener

--- snippets/js/automation/intercepted_request_event.js
+++ snippets/js/automation/intercepted_request_event.js
@@ -1,1 +1,3 @@
-(()=>{var {open:o,setRequestHeader:sh,send:sn}=p=XMLHttpRequest.prototype;p.open=function(m,u,a){var h={};p.setRequestHeader=(k,v)=>{h[k]=v;sh.call(this,k,v)};p.send=b=>{document.dispatchEvent(new CustomEvent('myreq',{detail:{method:m,url:u,headers:h,body:b}}));sn.call(this,b)};o.call(this,m,u,a)}})();
+(()=>{var{open:o,setRequestHeader:sh,send:sn}=p=XMLHttpRequest.prototype;p.open=function(m,u){var h={};p.setRequestHeader=(k,v)=>{h[k]=v;sh.call(this,k,v)};p.send=b=>{postMessage({method:m,url:u,headers:h,body:b},'*');sn.call(this,b)};o.call(this,m,u)}})();
+
+addEventListener('message',e=>{if(e.data.method)console.log(e.data)})
